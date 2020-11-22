8daca70fc0e53aedd444b78035fa1b2c536a44a0
CutlassS1968 <evanlloydjohns@gmail.com>
2020-11-22 13:42:24 -0500

    made really gross implementation of txt reading

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+#define MAXCHAR 1000
 int main(void) {
+
+    char* data[3000];
+
+    FILE *fp;
+    char str[MAXCHAR];
+    char* filename = "C:\\Users\\evanl\\CLionProjects\\CIS-241-FINAL-PROJECT\\SPY241Project.txt";
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s", filename);
+        return 1;
+    }
+
+    int c = 0;
+    while (fgets(str, MAXCHAR, fp)!= NULL) {
+        data[c] = str;
+        ++c;
+    }
+
+    fclose(fp);
+
     return 0;
 }
